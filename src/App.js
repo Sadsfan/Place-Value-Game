f119530404967ae665bfac77cf5e91ea4da2c4d0
Sadsfan <david.naughton2@gmail.com>
2025-07-11 07:37:29 +0100

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import GridNumberGame from './GridNumberGame';
+
+function App() {
+  return (
+    <div className="App">
+      <GridNumberGame />
+    </div>
+  );
+}
+
+export default App;
